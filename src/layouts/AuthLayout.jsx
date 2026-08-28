fd1e2b4d5978f9e36cb4a333bc249b32d65710c5
Chinawin Chueadee <chinawin.chueadee.work@gmail.com>
2026-08-28 16:48:02 +0700

    fix: to header

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,17 @@
+import AuthAside from "../components/auth/AuthAside";
 import Header from "../components/auth/AuthHeader";
 import { Outlet } from "react-router";
 
 function AuthLayout() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Header />
-      AuthLayout <Outlet />
+      <div className="grid flex-1 lg:grid-cols-2">
+        <AuthAside />
+        <div className="flex items-center justify-center p-6">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
