1eb0128fca0e7684f9d2d0f64460598d677ecbc0
Chinawin Chueadee <chinawin.chueadee.work@gmail.com>
2026-08-31 15:52:22 +0700

    progess Design

--- src/components/auth/AuthHeader.jsx
+++ src/components/auth/AuthHeader.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router";
-
-const NAV_LINKS = [
-  { label: "หน้าหลัก", to: "/" },
-  { label: "เกี่ยวกับเรา", to: "/about" },
-];
+import { Search } from "lucide-react";
 
 function Logo() {
   return (
@@ -13,8 +9,8 @@ function Logo() {
         {/* <span className="text-lg font-black text-accent">S</span> */}
       </div>
       <div>
-        <span className="text-lg font-black tracking-tight">SPEC</span>
-        <span className="text-lg font-black text-[#f97361] tracking-tight">
+        <span className="text-5xl font-black tracking-tight">SPEC</span>
+        <span className="text-5xl font-black text-[#f97316] tracking-tight">
           HUB
         </span>
       </div>
@@ -24,25 +20,78 @@ function Logo() {
 
 function Header() {
   return (
-    <header className="sticky top-0 w-full  shadow-sm  bg-white">
-      <div className="mx-auto grid max-w-7xl grid-cols-3 items-center px-4 py-3">
-        <Logo />
-        <nav className="flex items-center justify-center gap-10">
-          {NAV_LINKS.map(({ label, to }) => (
+    <header>
+      <div className="sticky top-0 w-full  shadow-sm  bg-[#fffff]">
+        <div className="mx-auto flex max-w-8xl justify-between items-center px-2 py-3 ">
+          <div className="px-30">
+            <Logo />
+          </div>
+
+          <form className="overflow-hidden border flex w-full max-w-lg rounded-lg bg-transparent text-sm border-black">
+            <input
+              type="text"
+              placeholder="ค้นหาสินค้า, แบรนด์, รุ่น..."
+              className="w-full px-5 py-2 rounded-sm outline-none"
+            />
+            <button
+              type="submit"
+              aria-label="ค้นหา"
+              className="px-5 py-2 flex items-center shrink-0 text-white bg-[#f97316] "
+            >
+              <Search size={20} />
+            </button>
+          </form>
+
+          <nav className="flex items-center justify-center max-w-full gap-10">
             <NavLink
-              key={to}
-              to={to}
+              to="/"
               className={({ isActive }) =>
-                `btn btn-ghost btn-sm rounded-field font-semibold normal-case ${
-                  isActive ? "text-accent" : "text-neutral"
+                `text-sm hover:text-[#f97316]  font-semibold normal-case ${
+                  isActive ? "text-[#f97316]" : "text-[#171717]"
                 }`
               }
             >
-              {label}
+              เปรียบเทียบ
             </NavLink>
-          ))}
-        </nav>
-        <div />
+
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                `text-sm hover:text-[#f97316]  font-semibold normal-case ${
+                  isActive ? "text-[#f97316]" : "text-[#171717]"
+                }`
+              }
+            >
+              รายการโปรด
+            </NavLink>
+
+            <NavLink
+              to="/store"
+              className={({ isActive }) =>
+                `text-sm hover:text-[#f97316]  font-semibold normal-case ${
+                  isActive
+                    ? "text-[#f97316] shadow-[#ea580c]"
+                    : "text-[#171717]"
+                }`
+              }
+            >
+              ตะกร้าสินค้า
+            </NavLink>
+
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                `text-sm hover:text-[#f97316]  font-semibold normal-case ${
+                  isActive
+                    ? "text-[#f97316] text-shadow-[#ea580c] text-shadow-3"
+                    : "text-[#171717]"
+                }`
+              }
+            >
+              เข้าสู่ระบบ / สมัตรสมาชิก
+            </NavLink>
+          </nav>
+        </div>
       </div>
     </header>
   );
